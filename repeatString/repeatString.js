adce37b07c30049dae78c7fcae1d271386daf731
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-05-29 01:44:08 +0200

    repeat String passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0){
+        return "ERROR";
+    }
+    let output = ""
+    for( let i = 0 ; i < number; i++){
+        output = output.concat(word);
+    }
+    return output;
 };
 
 module.exports = repeatString;
