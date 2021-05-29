8b872c408c016d1f71bf05969dd5fc04cd13be43
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-05-29 03:18:28 +0200

    leapYear passed

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if (yr % 4 === 0){
+        if (yr % 100 !== 0 || (yr % 100 ===0 && yr % 400 === 0)){
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 };
 
 module.exports = leapYears;
