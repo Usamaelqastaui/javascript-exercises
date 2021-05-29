2c9324764204f0d54d6ac277eb13acb3e72a1395
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-05-29 02:36:22 +0200

    reverse String passed

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let output = ""
+    let len = word.length - 1;
+    for (let i = len ; i >= 0; i--){
+        output += word.substr(i, 1);
+    }
+    return output;
 };
 
 module.exports = reverseString;
