8129e9de712ea4e32a87f65fc122cb8abc84bfb1
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-05-29 03:00:49 +0200

    removeFromArray Passed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(ary, ...Args) {
+    Args.forEach(function x(v, i , a){
+        let index = ary.indexOf(v);
+        if(index > -1){
+            ary.splice(index, 1);
+        }
+    })
+    return ary;
 };
 
 module.exports = removeFromArray;
