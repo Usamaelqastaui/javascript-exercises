e6934d0a791a3d16150414aa62ac9d14f73a41b7
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-06-05 01:30:26 +0200

    finished findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ppl) {
+    let preAge = 0
+    return ppl.reduce((a,b)=>{
+        let today = (new Date()).getFullYear();
+        let currentAge = 0;
+        (b['yearOfDeath'])? currentAge = b['yearOfDeath'] - b['yearOfBirth']: currentAge = today - b['yearOfBirth'] ;
+        if(currentAge > preAge){
+            a = b;
+            preAge = currentAge;
+        }
+        return a;
+    }, 0)
 };
 
 module.exports = findTheOldest;
