da85578c8951667b2c1504d0dd0b3f56fe82b215
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-06-04 22:08:04 +0200

    completed calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((acc,value)=>{
+    return acc += value;
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((acc, val)=>{
+    return acc *= val; 
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let sum = 1;
+  for(let i = 0; i < b; i++){
+    sum *= a
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  if(num === 0 || num === 1){
+    return 1;
+  } else {
+    let sum = 1;
+    for(let i = 1; i <= num; i++){
+      sum *= i;
+    }
+    return sum;
+  }
 };
 
 module.exports = {
