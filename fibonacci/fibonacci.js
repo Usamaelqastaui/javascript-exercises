0d918e05a9e611de349fd58a7135bdaaf88da6b9
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-06-04 23:09:41 +0200

    finished fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 1) {
+        return "OOPS"
+    } else if (a == 1 || a == 2){
+        return 1;
+    } else {
+        let sum = [1,1];
+        for(let i = 2; i < a; i++){
+            sum[i] = sum[i-1] + sum [i-2];
+        }
+        return sum[a-1];
+    }
+    
 };
 
 module.exports = fibonacci;
