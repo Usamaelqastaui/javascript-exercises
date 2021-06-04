3a84dee536fa10cb2181efe99d0f412baa790b63
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-06-04 22:51:11 +0200

    finished palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s]|_/g, "")
+             .replace(/\s+/g, "")
+             .toLowerCase();
 
+    let res = "";
+    const len = str.length;
+    for(let i = 0; i < len; i++){
+        res = res.concat(str[len-1-i]);
+    }
+    return res == str;
 };
 
 module.exports = palindromes;
